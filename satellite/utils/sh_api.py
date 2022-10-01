8c8ed823c26871fbad5307fe738c0f41402c0e02
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-01 08:07:40 +0000

    Adding tarfile member sanitization to extractall()

--- satellite/utils/sh_api.py
+++ satellite/utils/sh_api.py
@@ -110,7 +110,26 @@ class SHDownloader(abc.ABC):
         for tar in tars:
             tempdir = tempfile.mkdtemp(dir=Path(tar).parent)
             with tarfile.open(tar) as tarobj:
-                tarobj.extractall(tempdir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(tarobj, tempdir)
         if len(muls) >= 1:
             fd,tempmerged = tempfile.mkstemp(suffix=".tif")
             self._mosaic2one(muls,tempmerged)
